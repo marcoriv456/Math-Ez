11d8835c49ee8056c8837eb86f70924b1b8fa1c9
Marcos Rivera <marcosnicolasrivera456@gmail.com>
2024-09-04 15:19:51 -0600

    New tool section title color change animation
    A title color change animation has been added to the tool section, the title background color will change as the topics in the tool section passes by
    
    Other changes:
    The animation of the image in a tool section topic is now faster (from 1s to 0.5s )

--- home/js/anim-tool-section.js
+++ home/js/anim-tool-section.js
@@ -1,14 +1,40 @@
 export  default  function anim_init_tool_section(){
-    const callback=(entries)=>{
-        for (let entry of entries) {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("animated")
+
+    function anim_init__common(){
+        const callback=(entries)=> {
+            for (let entry of entries) {
+                if (entry.isIntersecting)
+                    entry.target.classList.add("animated")
             }
         }
+        const observer= new IntersectionObserver(callback, {rootMargin:"0px 10000px 0px 10000px"}   )
+        const items=document.querySelectorAll("#tool-section .to_animate")
+        items.forEach((item)=>observer.observe(item))
     }
-    const observer= new IntersectionObserver(callback, {rootMargin:"0px 10000px 0px 10000px"}   )
 
-    const animatedItems=document.querySelectorAll("#tool-section .to_animate")
-    animatedItems.forEach((item)=>observer.observe(item))
+    function anim_init__title_color_change(){
+        const title=document.querySelector("#tool-section .title");
+
+        const callback=(entries)=>{
+            for(let entry of entries){
+                const color=getComputedStyle(entry.target.querySelector(".panel")).getPropertyValue("--panel-theme-color");
+                if (entry.isIntersecting)
+                    title.style.backgroundColor=color
+            }
+        }
+
+        const observer=new IntersectionObserver(callback,{threshold:.4});
+
+        const items=document.querySelectorAll("#tool-section .panel-container")
+        for(let container of items){
+            observer.observe(container)
+        }
+    }
+
+    anim_init__common()
+    anim_init__title_color_change()
 }
+
+
+
 
